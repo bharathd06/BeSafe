7f87b9cd53916bfcd1e808c26d173c1934fa3f7b
Bharath D <bharathd2303@gmail.com>
2026-09-24 16:16:30 +0530

    conflict

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import os
 import requests
-
+#main
 app = Flask(__name__)
 RESULT_FOLDER = "static/results"
 os.makedirs(RESULT_FOLDER, exist_ok=True)
